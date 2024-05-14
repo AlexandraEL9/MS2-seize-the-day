57e5e1e61cd3f7f73c3f7a5d6d20e4d6eda79aef
AlexandraEL9 Lee <alexandraelee9@gmail.com>
2024-05-14 12:49:35 +0000

    feat:add function to update date and time with JS

---
+++ script.js
@@ -0,0 +1,27 @@
+//function to update current date and time
+function updateDateTime() {
+    //get current date and time
+    const currentDate = new Date();//creates object for date info
+    //say how date to be formatted
+    const options = {weekday: 'long', year: 'numeric', month: 'long', day: 'numeric'};
+    //format date
+    const formattedDate = currentDate.toLocaleDateString('en-GB', options);
+    //get current hrs and mins
+    const hours = String(currentDate.getHours()).padStart(2, '0');
+    const minutes = String(currentDate.getMinutes()).padStart(2, '0');
+    //format time as HH:MM
+    const formattedTime = `${hours}:${minutes}`;
+    //get element where date and time will be displayed
+    const currentDateTimeElement = document.getElementById('currentDateTime');
+    //set text content of the element
+    currentDateTimeElement.textContent = `${formattedDate} ${formattedTime}`;
+}
+
+// Function to update the date and time every second
+function updateTimeContinuously() {
+    updateDateTime();
+    setInterval(updateDateTime, 1000);
+}
+
+// Event listeners
+updateTimeContinuously();
