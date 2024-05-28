2680592e839f92ab39186df26aad31303d4d02d3
AlexandraEL9 Lee <alexandraelee9@gmail.com>
2024-05-28 14:23:35 +0000

    add comments

--- assets/js/error.js
+++ assets/js/error.js
@@ -1,16 +1,20 @@
-// JavaScript to redirect to index.html after 10 seconds
-var countdown = 10;
-var clockDisplay = document.getElementById("clock");
-
+/* global bootstrap, $ */
 function updateClock() {
+    // Update the text content of the clockDisplay element to show the current countdown value
     clockDisplay.textContent = "Countdown: " + countdown + " seconds";
+
+    // Decrement the countdown value by 1
     countdown--;
 
+    // Check if the countdown has reached below 0
     if (countdown < 0) {
+        // If countdown is below 0, redirect the browser to "index.html"
         window.location.href = "index.html";
     } else {
+        // If countdown is not below 0, call updateClock again after 1000 milliseconds (1 second)
         setTimeout(updateClock, 1000);
     }
 }
 
+// Start the countdown
 updateClock();
